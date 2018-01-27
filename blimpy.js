eca82b2a8dd9119e88710b7a4ed4d533da3534c8
Marco Burstein <theskunkmb@gmail.com>
2018-01-26 16:11:34 -0800

    Remove mutating functions.
    They don't actually work.

--- blimpy.js
+++ blimpy.js
@@ -15,17 +15,6 @@ blimpy.withClass(someClass, someObject) {
 }
 
 /**
- * Adds a class's prototype to an object by mutating it.
- *
- * @param {function} someClass The class to use.
- * @param {object} someObject The object to set the prototype of.
- * @returns {object} The object with the new prototype.
- */
-blimpy.setClass(someClass, someObject) {
-    return someObject = blimpy.withClass(someClass, someObject);
-}
-
-/**
  * "Removes" an object's prototype by setting it to the `Object` prototype
  * without mutating it.
  *
@@ -34,17 +23,6 @@ blimpy.setClass(someClass, someObject) {
  */
 blimpy.withNoClass(someObject) {
     return blimpy.withClass(Object, someObject);
-}
-
-/**
- * "Removes" an object's prototype by setting it to the `Object` prototype by
- * mutating it.
- *
- * @param {object} someObject The object to remove the prototype of.
- * @returns {object} The object without the prototype.
- */
-blimpy.setNoClass(someObject) {
-    return someObject = blimpy.withNoClass(someObject);
 }
 
 module.exports = blimpy;
